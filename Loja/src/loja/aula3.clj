b057f2139f086a845cf4c9e0d6448267e15a159e
Rafael Monteiro Porto <rafael.monteiro.porto@outlook.com>
2021-06-17 10:00:09 -0300

    feat(Coleções no dia a dia): Concluido aula 3.

--- Loja/src/loja/aula3.clj
+++ Loja/src/loja/aula3.clj
@@ -13,3 +13,65 @@
 (println (group-by minha-funcao-de-agrupamento (l.db/todos-os-pedidos)))
 
 (println (map count (vals (group-by :usuario (l.db/todos-os-pedidos)))))
+
+(->> (l.db/todos-os-pedidos)
+     (group-by :usuario)
+     vals
+     (map count)
+     println)
+
+(defn conta-total-por-usuario
+  [[usuario pedidos]]
+  (count pedidos))
+
+(->> (l.db/todos-os-pedidos)
+     (group-by :usuario)
+     (map conta-total-por-usuario)
+     println)
+
+(defn conta-total-por-usuario
+  [[usuario pedidos]]
+  [usuario (count pedidos)])
+
+(->> (l.db/todos-os-pedidos)
+     (group-by :usuario)
+     (map conta-total-por-usuario)
+     println)
+
+(defn conta-total-por-usuario
+  [[usuario pedidos]]
+  {:usuario-id usuario
+   :total-de-pedidos (count pedidos)})
+
+(->> (l.db/todos-os-pedidos)
+     (group-by :usuario)
+     (map conta-total-por-usuario)
+     println)
+
+(println "PEDIDOS")
+
+(defn total-do-item
+  [[_ detalhes]]
+  (* (get detalhes :quantidade 0) (get detalhes :preco-unitario 0)))
+
+(defn total-do-pedido
+  [pedido]
+  (reduce + (map total-do-item pedido)))
+
+(defn total-dos-pedidos
+  [pedidos]
+  (->> pedidos
+       (map :itens)
+       (map total-do-pedido)
+       (reduce +)))
+
+(defn quantia-de-pedidos-e-gasto-total-por-usuario
+  [[usuario pedidos]]
+  {:usuario-id usuario
+   :total-de-pedidos (count pedidos)
+   :preco-total (total-dos-pedidos pedidos)})
+
+(->> (l.db/todos-os-pedidos)
+     (group-by :usuario)
+     (map quantia-de-pedidos-e-gasto-total-por-usuario)
+     println)
