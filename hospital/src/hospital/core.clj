e66378e022510b43f7c74abcca964031505aad9c
Rafael Monteiro Porto <rafael.monteiro.porto@outlook.com>
2021-06-23 07:58:48 -0300

    feat(Mutabilidade com átomos e refs): Colocando limite manual na fila.

--- hospital/src/hospital/core.clj
+++ hospital/src/hospital/core.clj
@@ -1,7 +1,11 @@
-(ns hospital.core)
+(ns hospital.core
+  (:use [clojure pprint])
+  (:require [hospital.model :as h.model]))
 
 ;espera FILA DE ESPERA
-
 ;laboratorio1
 ;laboratorio2
 ;laboratorio3
+
+(let [hospital-do-gui (h.model/novo-hospital)]
+  (pprint hospital-do-gui))
---
+++ hospital/src/hospital/logic.clj
@@ -0,0 +1,19 @@
+(ns hospital.logic)
+
+(defn cabe-na-fila? [hospital departamento]
+  (-> hospital
+      (get departamento)
+      count
+      (>= 5)))
+
+(defn chega-em
+  [hospital departamento pessoa]
+  (let []
+    (if (cabe-na-fila? hospital departamento)
+      (update hospital departamento conj pessoa)
+      (throw (ex-info "Fila já está cheia" { :tentando-adicionar pessoa }))
+      )))
+
+(defn atende
+  [hospital departamento]
+  (update hospital departamento pop))
