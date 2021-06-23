e0f256e627c439d8b85d475ec4c008e679abcae0
Rafael Monteiro Porto <rafael.monteiro.porto@outlook.com>
2021-06-23 08:35:09 -0300

    feat(Mutabilidade com átomos e refs): O problema de várias threads

--- hospital/src/hospital/logic.clj
+++ hospital/src/hospital/logic.clj
@@ -4,15 +4,21 @@
   (-> hospital
       (get departamento)
       count
-      (>= 5)))
+      (< 5)))
 
 (defn chega-em
   [hospital departamento pessoa]
-  (let []
     (if (cabe-na-fila? hospital departamento)
       (update hospital departamento conj pessoa)
-      (throw (ex-info "Fila já está cheia" { :tentando-adicionar pessoa }))
-      )))
+      (throw (ex-info "Fila já está cheia" { :tentando-adicionar pessoa }))))
+
+(defn chega-em-pausado
+  [hospital departamento pessoa]
+  (Thread/sleep (* (rand) 2000))
+    (if (cabe-na-fila? hospital departamento)
+      (do
+          (update hospital departamento conj pessoa))
+      (throw (ex-info "Fila já está cheia" { :tentando-adicionar pessoa }))))
 
 (defn atende
   [hospital departamento]
