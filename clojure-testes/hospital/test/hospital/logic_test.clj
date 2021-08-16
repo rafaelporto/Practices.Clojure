d364733fa59e99f9537ab8394bfeaf2d94d005ca
Rafael <rafael.monteiro.porto@outlook.com>
2021-08-16 08:16:07 -0300

    Feat(Explorando testes): Aula 2
    Aulas:
    Testes e sequências
    Tratando erro com exceptions e nulo

--- clojure-testes/hospital/test/hospital/logic_test.clj
+++ clojure-testes/hospital/test/hospital/logic_test.clj
@@ -15,7 +15,15 @@
 
   ; borda do limite
   (testing "Que não cabe na fila quando a fila está cheia"
-    (is (not (cabe-na-fila? {:espera [1, 2, 3, 4, 5]}, :espera))))
+
+    ; é de simples leitura, pois é sequencial,
+    ; mas a desvantagem é que podemos errar em fazer coisas sequenciais
+    ;(is (not (cabe-na-fila? {:espera [1, 2, 3, 4, 5]}, :espera)))
+
+    ; não precisa ser sequencial e no mundo real não é
+    ; portanto faça testes que não são sequênciais
+    (is (not (cabe-na-fila? {:espera [1 5 37 54 21]}, :espera)))
+    )
 
   ; one off da borda do limiete para cima
   (testing "Que não cabe na fila quando tem mais de uma fila cheia"
@@ -25,6 +33,56 @@
     (is (cabe-na-fila? {:espera [1, 2, 3, 4]}, :espera))
     (is (cabe-na-fila? {:espera [1, 2]}, :espera)))
 
-  (testing "Quando o departamento não existe"
+  (testing "Que não cabe quando o departamento não existe"
     (is (not (cabe-na-fila? {:espera [1, 2, 3, 4]}, :raio-x))))
   )
+
+(deftest chega-em-test
+
+  (testing "aceita pessoas enquanto cabem pessoas na fila"
+
+    ; implentanção ruim, pois testa que escrevemos o que escrevemos,
+    ; isto é, testa o que erramos o que erramos e que acertamos o que acertamos
+    ;(is (= (update {:espera [1,2]} :espera 5)
+    ;       (chega-em {:espera [1,2]}, :espera, 5)))
+
+    (is (= { :espera [1,2,3,4,5] }
+           (chega-em {:espera [1, 2, 3, 4]}, :espera, 5)))
+    ; teste não sequencial
+    (is (= { :espera [1,2,5] }
+           (chega-em {:espera [1, 2]}, :espera, 5))))
+
+  (testing "não aceita quando não cabe a fila"
+    ; verificando que uma exception foi jogada
+    ; código clássico horrível. Usamos uma exception GENERICA.
+    ; mas qualquer outro erro genérico vai jogar essa exception, e nós vamos achar que deu certo
+    ; quando deu errado
+    ;(is (thrown? clojure.lang.ExceptionInfo
+    ;             (chega-em {:espera [1 35 42 64 21]}, :espera 76)))
+
+    ; mesmo que eu escolha uma exception do genero, perigoso!
+    ;(is (thrown? IllegalStateException
+    ;             (chega-em {:espera [1 35 42 64 21]}, :espera 76)))
+
+    ; outra abordagem, do nil
+    ; mas o perigo do swap, teriamos que trabalhar em outro ponto a condicao de erro
+    ;(is (nil? (chega-em {:espera [1 35 42 64 21]}, :espera 76)))
+
+    )
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
